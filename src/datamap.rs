5b391fc0ec1e67441ec4c50cbcaf7bf5cb05dde0
Nova <technobaboo@gmail.com>
2023-09-07 23:59:42 -0400

    refactor(datamap): use flexbuffers convenience functions

--- src/datamap.rs
+++ src/datamap.rs
@@ -1,34 +1,27 @@
 use serde::{de::DeserializeOwned, Serialize};
 use stardust_xr_fusion::{
-	core::schemas::flex::flexbuffers::{DeserializationError, FlexbufferSerializer, Reader},
+	core::schemas::flex::flexbuffers::{self, DeserializationError},
 	input::InputMethod,
 	node::NodeError,
 };
 
-pub struct Datamap<D: Serialize + DeserializeOwned> {
-	serializer: FlexbufferSerializer,
-	pub data: D,
-}
+pub struct Datamap<D: Serialize + DeserializeOwned>(D);
 impl<D: Serialize + DeserializeOwned> Datamap<D> {
 	pub fn create(data: D) -> Self {
-		Datamap {
-			serializer: FlexbufferSerializer::new(),
-			data,
-		}
+		Datamap(data)
 	}
 	pub fn from_data(data: &[u8]) -> Result<Self, DeserializationError> {
-		let data = D::deserialize(Reader::get_root(data)?)?;
-		Ok(Datamap {
-			serializer: FlexbufferSerializer::new(),
-			data,
-		})
+		flexbuffers::from_slice(data).map(|f| Datamap(f))
 	}
 	pub fn update_input_method(&mut self, input_method: &InputMethod) -> Result<(), NodeError> {
 		input_method.set_datamap(&self.serialize())
 	}
 
+	pub fn data(&mut self) -> &mut D {
+		&mut self.0
+	}
+
 	pub fn serialize(&mut self) -> Vec<u8> {
-		self.data.serialize(&mut self.serializer).unwrap();
-		self.serializer.take_buffer()
+		flexbuffers::to_vec(&mut self.data()).unwrap()
 	}
 }
