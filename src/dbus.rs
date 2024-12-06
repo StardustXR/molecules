b5e5c9948c82a1c27cf5f200254e6c77023b0ef7
Nova <technobaboo@gmail.com>
2024-12-06 00:44:21 -0500

    feat(keyboard): use dbus

--- src/dbus.rs
+++ src/dbus.rs
@@ -1,9 +1,10 @@
 use stardust_xr_fusion::core::schemas::zbus::Connection;
 use std::{any::Any, marker::PhantomData};
+use tokio::task::AbortHandle;
 use zbus::{object_server::Interface, zvariant::OwnedObjectPath};
 
 #[allow(dead_code)]
-pub struct DbusObjectHandles(pub(crate) Box<dyn Any>);
+pub struct DbusObjectHandles(pub(crate) Box<dyn Any + Send + Sync + 'static>);
 
 pub struct DbusObjectHandle<I: Interface>(
 	pub(crate) Connection,
@@ -15,11 +16,14 @@ impl<I: Interface> Drop for DbusObjectHandle<I> {
 		let connection = self.0.clone();
 		let object_path = self.1.clone();
 		tokio::task::spawn(async move {
-			connection
-				.object_server()
-				.remove::<I, _>(object_path)
-				.await
-				.unwrap();
+			let _ = connection.object_server().remove::<I, _>(object_path).await;
 		});
+	}
+}
+
+pub struct AbortOnDrop(pub AbortHandle);
+impl Drop for AbortOnDrop {
+	fn drop(&mut self) {
+		self.0.abort();
 	}
 }
--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,5 +1,6 @@
-use crate::dbus::{DbusObjectHandle, DbusObjectHandles};
-use rustc_hash::FxHashMap;
+use crate::dbus::{AbortOnDrop, DbusObjectHandle, DbusObjectHandles};
+use futures_util::StreamExt;
+use rustc_hash::{FxHashMap, FxHashSet};
 use stardust_xr_fusion::{
 	core::schemas::zbus::{self, Connection},
 	fields::Field,
@@ -8,12 +9,18 @@ use stardust_xr_fusion::{
 	spatial::Spatial,
 };
 use std::marker::PhantomData;
-use zbus::{message::Header, names::UniqueName};
+use zbus::{
+	fdo,
+	message::Header,
+	names::{BusName, UniqueName},
+};
 
 pub struct KeyboardHandler {
 	keymap_ids: FxHashMap<UniqueName<'static>, u64>,
+	pressed_keys: FxHashMap<UniqueName<'static>, FxHashSet<u32>>,
 	on_key: Box<dyn Fn(u64, u32, bool) + Send + Sync + 'static>,
 }
+
 impl KeyboardHandler {
 	pub fn init<F: Fn(u64, u32, bool) + Send + Sync + 'static>(
 		connection: Connection,
@@ -22,54 +29,96 @@ impl KeyboardHandler {
 		on_key: F,
 	) -> DbusObjectHandles {
 		let path = random_object_name();
-		let path_clone = path.clone();
 
-		let connection_clone = connection.clone();
-		let connection_point = connection_point.cloned();
-		let field = field.clone();
-		tokio::spawn(async move {
-			let task_1 = async {
-				let field_object = FieldObject::new(field.clone()).await.unwrap();
-				connection_clone
-					.object_server()
-					.at(path_clone.clone(), field_object)
-					.await
-					.unwrap();
-			};
-			let task_2 = async {
-				if let Some(spatial) = connection_point {
-					let spatial_object = SpatialObject::new(spatial.clone()).await.unwrap();
-					connection_clone
+		let handler = KeyboardHandler {
+			keymap_ids: FxHashMap::default(),
+			pressed_keys: FxHashMap::default(),
+			on_key: Box::new(on_key),
+		};
+
+		let abort_handle = tokio::spawn({
+			let connection_point = connection_point.cloned();
+			let field = field.clone();
+			let path = path.clone();
+			let connection = connection.clone();
+
+			async move {
+				let task_1 = async {
+					let field_object = FieldObject::new(field.clone()).await.unwrap();
+					connection
 						.object_server()
-						.at(path_clone.clone(), spatial_object)
+						.at(path.clone(), field_object)
 						.await
 						.unwrap();
-				}
-			};
-			let task_3 = async {
-				connection_clone
-					.object_server()
-					.at(
-						path_clone.clone(),
-						KeyboardHandler {
-							keymap_ids: FxHashMap::default(),
-							on_key: Box::new(on_key),
-						},
-					)
-					.await
-					.unwrap();
-			};
+				};
+				let task_2 = async {
+					if let Some(spatial) = connection_point {
+						let spatial_object = SpatialObject::new(spatial.clone()).await.unwrap();
+						connection
+							.object_server()
+							.at(path.clone(), spatial_object)
+							.await
+							.unwrap();
+					}
+				};
+				let task_3 = async {
+					connection
+						.object_server()
+						.at(path.clone(), handler)
+						.await
+						.unwrap();
+				};
 
-			tokio::join!(task_1, task_2, task_3);
-		});
+				tokio::join!(task_1, task_2, task_3);
+
+				let Ok(dbus_proxy) = fdo::DBusProxy::new(&connection).await else {
+					return;
+				};
+				let Ok(mut name_changes) = dbus_proxy.receive_name_owner_changed().await else {
+					return;
+				};
+				while let Some(signal) = name_changes.next().await {
+					let args = signal.args().unwrap();
+
+					if args.new_owner.is_none() {
+						let BusName::Unique(bus) = args.name else {
+							continue;
+						};
+						let Ok(keyboard_handler) = connection
+							.object_server()
+							.interface::<_, KeyboardHandler>(&path)
+							.await
+						else {
+							continue;
+						};
+						keyboard_handler.get_mut().await.reset_keys(bus.to_owned());
+					}
+				}
+			}
+		})
+		.abort_handle();
 
 		DbusObjectHandles(Box::new((
+			AbortOnDrop(abort_handle),
 			DbusObjectHandle::<KeyboardHandler>(connection.clone(), path.clone(), PhantomData),
 			DbusObjectHandle::<SpatialObject>(connection.clone(), path.clone(), PhantomData),
 			DbusObjectHandle::<FieldObject>(connection, path, PhantomData),
 		)))
 	}
+
+	fn reset_keys(&mut self, sender: UniqueName<'static>) {
+		let Some(keymap_id) = self.keymap_ids.remove(&sender) else {
+			return;
+		};
+		let Some(keys) = self.pressed_keys.remove(&sender) else {
+			return;
+		};
+		for key in keys {
+			(self.on_key)(keymap_id, key, false);
+		}
+	}
 }
+
 #[zbus::interface(name = "org.stardustxr.XKBv1", proxy())]
 impl KeyboardHandler {
 	#[zbus(proxy(no_reply))]
@@ -81,19 +130,38 @@ impl KeyboardHandler {
 		let Some(sender) = header.sender() else {
 			return Ok(());
 		};
-		self.keymap_ids.insert(sender.to_owned(), keymap_id);
+		let sender = sender.to_owned();
+		self.keymap_ids.insert(sender.clone(), keymap_id);
 		Ok(())
 	}
+
 	#[zbus(proxy(no_reply))]
 	fn key_state(&mut self, #[zbus(header)] header: Header<'_>, key: u32, pressed: bool) {
 		let Some(sender) = header.sender() else {
 			return;
 		};
-		let Some(keymap_id) = self.keymap_ids.get(sender) else {
+		let sender = sender.to_owned();
+		let Some(keymap_id) = self.keymap_ids.get(&sender) else {
 			return;
 		};
 
+		let sender_entry = self.pressed_keys.entry(sender).or_default();
+		if pressed {
+			sender_entry.insert(key);
+		} else {
+			sender_entry.remove(&key);
+		}
+
 		(self.on_key)(*keymap_id, key, pressed)
+	}
+
+	#[zbus(proxy(no_reply))]
+	fn reset(&mut self, #[zbus(header)] header: Header<'_>) {
+		let Some(sender) = header.sender() else {
+			return;
+		};
+		let sender = sender.to_owned();
+		self.reset_keys(sender)
 	}
 }
 
@@ -133,21 +201,27 @@ async fn keyboard() {
 	)
 	.unwrap();
 
+	let pressed_notifier = std::sync::Arc::new(tokio::sync::Notify::new());
+	let _object_keyboard_handler =
+		KeyboardHandler::init(connect_client().await.unwrap(), None, &field, {
+			let pressed_notifier = pressed_notifier.clone();
+			move |keymap_id, key, pressed| {
+				if pressed {
+					println!("key {key} pressed")
+				} else {
+					println!("key {key} unpressed")
+				};
+				assert_eq!(keymap_id, 20);
+				assert_eq!(key, 10);
+				if pressed {
+					pressed_notifier.notify_waiters();
+				} else {
+					std::process::exit(0);
+				}
+			}
+		});
+
 	let connection = connect_client().await.unwrap();
-
-	let _keyboard_objects = KeyboardHandler::init(
-		connection.clone(),
-		None,
-		&field,
-		move |keymap_id, key, pressed| {
-			println!("key pressed");
-			assert_eq!(keymap_id, 20);
-			assert_eq!(key, 10);
-			assert!(pressed);
-			std::process::exit(0);
-		},
-	);
-
 	let object_registry = object_registry::ObjectRegistry::new(&connection)
 		.await
 		.unwrap();
@@ -166,5 +240,9 @@ async fn keyboard() {
 			keyboard_handler.key_state(10, true).await.unwrap();
 		});
 	}
+	pressed_notifier.notified().await;
+	drop(object_registry);
+	drop(connection);
+	println!("dropped object keyboard handler");
 	let _ = client.sync_event_loop(|_, _| {}).await;
 }
